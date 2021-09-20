0fb0dd9467253b34ff93c245bbaf27a4e5b44cde
Shahid0120 <shahid.hussain0120@gmail.com>
2021-09-20 14:19:59 +1000

    make test for basic jest syntax for objects, strings, numbers and arrays

--- testing_tutorial_traverse_media/functions.js
+++ testing_tutorial_traverse_media/functions.js
@@ -1,5 +1,15 @@
-const functions = { 
-    add: (num1, num2) => num1 + num2
-}
+const functions = {
+  add: (num1, num2) => num1 + num2,
+  isNull: () => null,
+  createUser : () => {
+      const user = {firstName: "Shahid"}
+      user["lastName"] = "Hussain";
+      return user;
+  },
+  containerWeight: () => {
+      return 1600;
+  }
+};
 
 module.exports = functions;
+
