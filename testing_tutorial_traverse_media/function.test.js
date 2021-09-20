ff23c02d14f6f370ad2cbbe48b3120eeb57c6a43
Shahid0120 <shahid.hussain0120@gmail.com>
2021-09-20 14:37:11 +1000

    make test holders suing json placeholder api and asynchornous calls and async await

--- testing_tutorial_traverse_media/function.test.js
+++ testing_tutorial_traverse_media/function.test.js
@@ -1,32 +1,48 @@
 const functions = require("./functions");
 
-
-test('Adds 2 plus 2 to equal 4', () => {
-    expect(functions.add(2,2)).toBe(4);
+test("Adds 2 plus 2 to equal 4", () => {
+  expect(functions.add(2, 2)).toBe(4);
 });
 
 test("Adds 2 plus 2 to NOT equal 5", () => {
   expect(functions.add(2, 2)).not.toBe(5);
 });
 
-
 test("Retrurns a null value", () => {
-    expect(functions.isNull()).toBeNull();
-})
+  expect(functions.isNull()).toBeNull();
+});
 
 test("Return the user first name and last name", () => {
-    expect(functions.createUser()).toMatchObject({
-      firstName: "Shahid",
-      lastName: "Hussain",
-    });
+  expect(functions.createUser()).toMatchObject({
+    firstName: "Shahid",
+    lastName: "Hussain",
+  });
 });
 
 // testing for the weight of the container to be below 1600
 
 test("Weight of container is < 1600", () => {
-    expect(functions.containerWeight()).toBeLessThanOrEqual(1600)
-})
+  expect(functions.containerWeight()).toBeLessThanOrEqual(1600);
+});
 
 test("there should not be a z in Shahid", () => {
-    expect("Shahidz").not.toMatch(/Z/);
+  expect("Shahidz").not.toMatch(/Z/);
+});
+//asyncrynours calls
+// api test  for asyncrynours calls
+// test("Userfetched name should be leanne Graham", () => {
+//   // this make sure that the api is fetched
+//   expect.assertions(1);
+//   return functions.fetchUser().then(data => {
+//     expect(data.username).toEqual("Bret");
+//   });
+// });
+
+// Async Await
+test("Userfetched name should be leanne Graham", async () => {
+  // this make sure that the api is fetched
+    expect.assertions(1);
+    const data = await functions.fetchUser()
+    expect(data.username).toEqual("Bret");
+    
 });
--- testing_tutorial_traverse_media/functions.js
+++ testing_tutorial_traverse_media/functions.js
@@ -1,15 +1,21 @@
+const axios = require("axios");
+
 const functions = {
   add: (num1, num2) => num1 + num2,
   isNull: () => null,
-  createUser : () => {
-      const user = {firstName: "Shahid"}
-      user["lastName"] = "Hussain";
-      return user;
+  createUser: () => {
+    const user = { firstName: "Shahid" };
+    user["lastName"] = "Hussain";
+    return user;
   },
   containerWeight: () => {
-      return 1600;
-  }
+    return 1600;
+  },
+  fetchUser: () => 
+    axios
+      .get("https://jsonplaceholder.typicode.com/users/1")
+      .then((res) => res.data)
+      .catch((err) => "error")
 };
 
 module.exports = functions;
-
